893bdb78880b6054823072441fa62f2786697b94
= <=>
2022-11-04 14:46:46 +0200

    changed type of time from long to timestamp

--- app/src/main/java/com/example/novadebug/model/clinic/reservation/Reservation.kt
+++ app/src/main/java/com/example/novadebug/model/clinic/reservation/Reservation.kt
@@ -1,16 +1,17 @@
 package com.example.novadebug.model.clinic.reservation
 
 import com.example.novadebug.model.clinic.Doctor
+import com.google.firebase.Timestamp
 
 data class Reservation(
     val id: String = "",
     val area: ArrayList<String> = ArrayList(),
     val branch: String = "",
     val device: String = "",
-    val doctor: Doctor,
+    val doctor: Doctor = Doctor(),
     val status: String = "",
-    val time: Long,
+    val time: Timestamp = Timestamp.now(),
     val userId: String = "",
-    val expectedDuration: Int,
+    val expectedDuration: Int = 0,
 )
 
