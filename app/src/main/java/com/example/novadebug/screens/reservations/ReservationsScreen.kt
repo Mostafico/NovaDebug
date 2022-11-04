94775b14041210adb7789027aef67244721544eb
= <=>
2022-11-04 10:43:13 +0200

    passed the new parameters into the reservation instances @ the preview

--- app/src/main/java/com/example/novadebug/screens/reservations/ReservationsScreen.kt
+++ app/src/main/java/com/example/novadebug/screens/reservations/ReservationsScreen.kt
@@ -20,12 +20,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.novadebug.model.clinic.Doctor
 import com.example.novadebug.model.clinic.reservation.Reservation
 
 @Composable
 fun ReservationScreen(
     reservationsViewModel: ReservationsViewModel = hiltViewModel(),
-    navController: NavHostController
+    navController: NavHostController,
 ) {
     LaunchedEffect(key1 = true) {
         reservationsViewModel.updateReservations()
@@ -57,7 +58,9 @@ fun ReservationScreenContent(items: List<Reservation>, isLoading: Boolean) {
 @Composable
 fun ReservationList(items: List<Reservation>) {
     LazyColumn(
-        modifier = Modifier.padding(16.dp).fillMaxSize(),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(items = items) { item ->
@@ -110,49 +113,30 @@ fun ReservationCardItem(reservation: Reservation) {
 @Preview
 @Composable
 fun ReservationListPreview() {
+    val reservation = Reservation(
+        id = "1FRFZpSa7fWPlsaYJc7E",
+        area = arrayListOf(
+            "area1",
+            "area2"
+        ),
+        branch = "smoha",
+        device = "candela",
+        doctor = Doctor(
+            id = "RfCDM6o8knwSElvVhK33"
+        ),
+        status = "scheduled",
+        time = System.currentTimeMillis(),
+        userId = "o2SG28pYMx8TgicYdTlu",
+        expectedDuration = 30
+    )
+    val reservationsList = listOf(
+        reservation,
+        reservation.copy(status = "canceled"),
+        reservation.copy(expectedDuration = 15),
+        reservation.copy(status = "canceled")
+    )
     ReservationList(
-        items = listOf(
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            ),
-            Reservation(
-                id = "1FRFZpSa7fWPlsaYJc7E",
-                device = "Cadiac",
-                branch = "Smouha"
-            )
-        )
+        reservationsList
     )
 }
 
@@ -162,8 +146,19 @@ fun ReservationCardItemPreview() {
     ReservationCardItem(
         reservation = Reservation(
             id = "1FRFZpSa7fWPlsaYJc7E",
-            device = "Cadiac",
-            branch = "Smouha"
+            area = arrayListOf(
+                "area1",
+                "area2"
+            ),
+            branch = "smoha",
+            device = "candela",
+            doctor = Doctor(
+                id = "RfCDM6o8knwSElvVhK33"
+            ),
+            status = "scheduled",
+            time = System.currentTimeMillis(),
+            userId = "o2SG28pYMx8TgicYdTlu",
+            expectedDuration = 30
         )
     )
 }
