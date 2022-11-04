77f34fa5aee001ad1160800cc69cc6a1490534fe
= <=>
2022-11-04 10:21:23 +0200

    Added new fields

--- app/src/main/java/com/example/novadebug/model/clinic/reservation/Reservation.kt
+++ app/src/main/java/com/example/novadebug/model/clinic/reservation/Reservation.kt
@@ -1,7 +1,15 @@
 package com.example.novadebug.model.clinic.reservation
 
+import com.example.novadebug.model.clinic.Doctor
+
 data class Reservation(
     val id: String = "",
+    val area: ArrayList<String> = ArrayList(),
     val branch: String = "",
-    val device: String = ""
+    val device: String = "",
+    val doctor: Doctor,
+    val status: String = "",
+    val time: Long,
+    val userId: String = "",
 )
+
