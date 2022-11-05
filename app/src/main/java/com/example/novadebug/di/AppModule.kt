47061d2fed14f74651638075a2ddcc509eda4b5a
= <=>
2022-11-05 18:46:12 +0200

    provided repository @ module

--- app/src/main/java/com/example/novadebug/di/AppModule.kt
+++ app/src/main/java/com/example/novadebug/di/AppModule.kt
@@ -3,6 +3,9 @@ package com.example.novadebug.di
 import com.example.novadebug.common.CLIENTS_COLLECTION
 import com.example.novadebug.common.CLIENT_NAME
 import com.example.novadebug.common.CLIENT_RESERVATIONS
+import com.example.novadebug.data.repository.ClinicRepositoryImpl
+import com.example.novadebug.domain.repository.ClinicRepository
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -21,4 +24,9 @@ object AppModule {
     fun provideReservationsRef(
         db: FirebaseFirestore,
     ) = db.collection(CLIENTS_COLLECTION).document(CLIENT_NAME).collection(CLIENT_RESERVATIONS)
+
+    @Provides
+    fun provideClinicRepository(
+        reservationsRef: CollectionReference,
+    ): ClinicRepository = ClinicRepositoryImpl(reservationsRef)
 }
