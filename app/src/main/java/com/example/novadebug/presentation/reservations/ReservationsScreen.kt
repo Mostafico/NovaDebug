7ecfc018a037475f2bfd5c13351b35733317d6b8
= <=>
2022-11-06 00:57:17 +0200

    Implemented add reservations components

--- app/src/main/java/com/example/novadebug/presentation/reservations/ReservationsScreen.kt
+++ app/src/main/java/com/example/novadebug/presentation/reservations/ReservationsScreen.kt
@@ -3,13 +3,32 @@ package com.example.novadebug.presentation.reservations
 
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.novadebug.common.components.TopBar
+import com.example.novadebug.domain.model.clinic.Doctor
+import com.example.novadebug.domain.model.clinic.reservation.Reservation
+import com.example.novadebug.presentation.reservations.components.AddReservation
+import com.example.novadebug.presentation.reservations.components.AddReservationFab
 import com.example.novadebug.presentation.reservations.components.ReservationContent
 import com.example.novadebug.presentation.reservations.components.Reservations
+import com.google.firebase.Timestamp
 
+var dummyReservation = Reservation(
+    id = "",
+    area = arrayListOf("area 1", "area 2"),
+    branch = "roshdi",
+    device = "candela",
+    doctor = Doctor(id = "22222"),
+    status = "scheduled",
+    time = Timestamp.now(),
+    userId = "44444",
+    expectedDuration = 30
+)
 
 @Composable
-fun ReservationScreen() {
+fun ReservationScreen(
+    viewModel: ReservationsViewModel = hiltViewModel(),
+) {
     Scaffold(
         topBar = { TopBar() },
         content = { padding ->
@@ -22,7 +41,13 @@ fun ReservationScreen() {
                 }
             )
 
+        },
+        floatingActionButton = {
+            AddReservationFab(
+                addReservation = { viewModel.addReservation(dummyReservation) }
+            )
         }
     )
+    AddReservation()
 }
 
